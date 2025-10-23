0479720340947be0e8c558d8a6913d387b82421a
C-H-Genesis <chrishaston2@gmail.com>
2025-10-23 11:58:28 +0200

    Update Connection String

--- obj/Debug/net8.0/SMS.AssemblyInfo.cs
+++ obj/Debug/net8.0/SMS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c444d1242de5e7cbaad9037c40a369ec35d059e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c349876b2f2024be82cc35b751ee7a8f8cdf3260")]
 [assembly: System.Reflection.AssemblyProductAttribute("SMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
